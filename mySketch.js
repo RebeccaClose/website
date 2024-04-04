b6d6e78a87127d8d44eebf2da8ecbda4bee95d32
RebeccaClose <121636564+RebeccaClose@users.noreply.github.com>
2024-04-05 01:02:42 +0200

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -5,10 +5,6 @@ function setup() {
   background(0, 0, 255);
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
-
 function draw() {
 	strokeWeight (10)
   stroke(255, 255, 255);
