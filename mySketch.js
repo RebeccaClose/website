6ed8ff3b62a3f3fd3b3f18f4904ac2b82906e04b
RebeccaClose <121636564+RebeccaClose@users.noreply.github.com>
2024-06-15 19:34:08 +0200

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -6,11 +6,11 @@ function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position (0,0);
   canvas.style('z-index','-1');
-  background(0, 0, 255);
-}
+  background(255, 0, 153);
+} 
 
 function draw() {
-	strokeWeight (100)
+	strokeWeight (80)
   stroke(255, 255, 255);
   {
     line(mouseX, mouseY, pmouseX, pmouseY);
